739531521955d17ad28f206c210b85eb23f4da0a
Tireon003 <gunash29@mail.ru>
2024-06-29 13:39:26 +0300

    rebased

--- scrapper.py
+++ scrapper.py
@@ -1,7 +1,5 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.wait import WebDriverWait
 from selenium.common.exceptions import NoSuchElementException
 from datetime import timedelta, date
 import time

d515ff03ba8ce3366984367ef6c9802da5d4c994
Tireon003 <gunash29@mail.ru>
2024-07-20 15:31:18 +0300

    replaced titles and elements storing into state

--- handlers/category_parser.py
+++ handlers/category_parser.py
@@ -1,39 +1,31 @@
-import json
-
-from aiogram import Router, types, F
+from aiogram import Router, types
 from aiogram.filters import Command, StateFilter
 from aiogram.fsm.context import FSMContext
 from aiogram.types import ReplyKeyboardRemove
 
 from keyboards.select_category_keyboard import categories_keyboard
 from keyboards.select_subcategory_keyboard import subcategories_keyboard
-from modules.scrapper import WebDriverManager, CategoryParser, PageDataParser
+from modules.scrapper import WebDriverManager, CategoryParser
 from states.parse_category_state import ParseCategoryState
 
 router = Router()
 
-# todo перенести хранение данных в state
-# если оставить так, то когда 2 и более юзера будут пользоваться парсом категории будет либо ошибка либо будет парс неверной категории
-category_label_elements: list = []
-categories_title: list[str] = []
-subcategory_label_elements: list = []
-subcategories_title: list[str] = []
+# TODO: Сделать так, чтобы можно было парсить категорию нескольким пользователям одновременно
+
 driver: WebDriverManager
 category_parser: CategoryParser
 
 
 @router.message(Command("category"), StateFilter(None))
 async def init_category_parse(message: types.Message, state: FSMContext):
-    global category_label_elements, \
-        categories_title, \
-        driver, \
-        category_parser
+    global driver, category_parser
 
     driver = WebDriverManager()
     category_parser = CategoryParser(driver.init_webdriver())
     category_label_elements = category_parser.get_category_list()
     categories_title = list([el.text for el in category_label_elements])
-    print(categories_title)
+
+    await state.update_data(cat_titles=categories_title, cat_elements=category_label_elements)
 
     await message.answer(
         text="Выберите категорию:",
@@ -42,18 +34,17 @@ async def init_category_parse(message: types.Message, state: FSMContext):
     await state.set_state(ParseCategoryState.choosing_category)
 
 
-@router.message(ParseCategoryState.choosing_category)  # (, F.text.in_(categories_title))
+@router.message(ParseCategoryState.choosing_category)
 async def category_is_chosen(message: types.Message, state: FSMContext):
-    global category_label_elements, \
-        categories_title, \
-        driver, \
-        category_parser, \
-        subcategories_title, \
-        subcategory_label_elements
-    if message.text in categories_title:
-        category_parser.set_category(category_label_elements[categories_title.index(message.text)])
+    global driver, category_parser
+
+    state_data = await state.get_data()
+
+    if message.text in state_data['cat_titles']:
+        category_parser.set_category(state_data['cat_elements'][state_data['cat_titles'].index(message.text)])
         subcategory_label_elements = category_parser.get_subcategories()
         subcategories_title = [el.text.strip() for el in subcategory_label_elements]
+        await state.update_data(subcat_titles=subcategories_title, subcat_elements=subcategory_label_elements)
         await message.answer(
             text="Отлично, теперь выберите подкатегорию:",
             reply_markup=subcategories_keyboard(subcategories_title)
@@ -71,13 +62,11 @@ async def category_is_chosen(message: types.Message, state: FSMContext):
 
 @router.message(ParseCategoryState.choosing_subcategory)
 async def subcategory_is_chosen(message: types.Message, state: FSMContext):
-    global driver, \
-        category_parser, \
-        subcategories_title, \
-        subcategory_label_elements
-    if message.text in subcategories_title:
+    global driver, category_parser
+    state_data = await state.get_data()
+    if message.text in state_data['subcat_titles']:
         category_url = category_parser.set_subcategory(
-            selected_subcategory_element=subcategory_label_elements[subcategories_title.index(message.text)]
+            selected_subcategory_element=state_data['subcat_elements'][state_data['subcat_titles'].index(message.text)]
         )
         await state.update_data(url=category_url)
         await message.answer(
@@ -102,7 +91,7 @@ async def number_of_products_entered(message: types.Message, state: FSMContext):
     state_data = await state.get_data()
     products_number = message.text
     category_url = state_data['url']
-    if not products_number.isdigit() or 1 > int(products_number) > 100:  # todo условие неверно отрабаывает, переписать
+    if not products_number.isdigit() or 1 > int(products_number) > 100:
         await message.answer(
             text="Введен неверный формат числа. Отмена операции...",
             reply_markup=ReplyKeyboardRemove()
